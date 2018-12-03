f6c5779d4bd041ecc750fe841adbbc03c5f0bc74
Hubert Bryłkowski <hubert.brylkowski@brainly.com>
2018-12-03 17:48:12 +0100

    refactor

--- 03/solution.py
+++ 03/solution.py
@@ -1,5 +1,8 @@
 import re
 from dataclasses import dataclass
+from typing import Set, List
+
+MAX_FABRIC_SIZE = 1000
 
 
 @dataclass
@@ -10,7 +13,7 @@ class Patch:
     width: int
     height: int
 
-    def get_fields(self):
+    def covered_patches(self):
         for row in range(self.from_left, self.from_left + self.width):
             for column in range(self.from_top, self.from_top + self.height):
                 yield (row, column)
@@ -20,31 +23,32 @@ with open('input') as f:
     raw_patches = f.read().splitlines()
 
 
+splitter = re.compile('[^0-9]')
 patches = []
-regex = re.compile('[^0-9]')
-for patch in raw_patches:
-    patches.append(Patch(*[int(n) for n in regex.split(patch) if n != '']))
+for raw_patch in raw_patches:
+    patches.append(Patch(*[int(n) for n in splitter.split(raw_patch) if n != '']))
 
-fabric = [[set() for _ in range(1000)] for _ in range(1000)]
+fabric: List[List[Set]] = [[set() for _ in range(MAX_FABRIC_SIZE)] for _ in range(MAX_FABRIC_SIZE)]
+
 
 for patch in patches:
-    for field in patch.get_fields():
+    for field in patch.covered_patches():
         y, x = field[0], field[1]
         fabric[x][y].add(patch.id)
 
 
 count = 0
-non_overlaping = set([patch.id for patch in patches])
+non_overlapping = set([patch.id for patch in patches])
 for x in fabric:
     for y in x:
         if len(y) > 1:
             count += 1
             for p in y:
                 try:
-                    non_overlaping.remove(p)
+                    non_overlapping.remove(p)
                 except KeyError:
                     pass
 
 print(count)
-print(non_overlaping)
+print(non_overlapping)
 
